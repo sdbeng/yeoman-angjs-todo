4f7cc082c12b4dc3762f5e2f7d9de13c767cf046
Sergio Barrera <sdbeng@gmail.com>
2014-09-07 16:05:42 -0700

    Update views in main template

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,10 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Go to San Jose',
+      'Buy a mbpro in San Francisco',
+      'Fix bank accounts',
+      'Pay rent'
     ];
   });
