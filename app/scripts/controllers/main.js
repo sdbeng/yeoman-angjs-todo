ca9a25ca2cc52ec24d661148b96e89ef4b924539
Sergio Barrera <sdbeng@gmail.com>
2014-09-08 08:06:17 -0700

    Add remove button form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,4 +19,9 @@ angular.module('mytodoApp')
     	$scope.todos.push($scope.todo);
     	$scope.todo = '';
     };
+    //add some logic for removing todo items to our controller
+    $scope.removeTodo = function(index) {
+    	//removes 1 todo item from items array using the splice() method at the given $index value.
+    	$scope.todos.splice(index, 1);
+    };
   });
